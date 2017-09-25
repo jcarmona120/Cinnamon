ab118a24e627e0fd0e190999e7df0d41eed547ed
Jawann Carmona <jcarmona@ACTNY-IT-Jawann-Carmona.local>
2017-09-25 17:14:40 -0400

    if statement for printing first synonym to screen

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,24 +19,38 @@ class Search extends Component {
     }
 
     handleSubmit(event) {
-        axios.get(`https://api.datamuse.com/words?ml=${this.state.searchTerm}`)
+        axios.get(`https://api.datamuse.com/words?md=d&ml=${this.state.searchTerm}`)
             .then((response) => {
             	this.setState({wordData: response.data})
-                var words = response.data;
-                console.log(words)
+                console.log(this.state.wordData[0])
             })
         event.preventDefault();
     }
 
+    
+
     render() {
-        return ( 
-        <form onSubmit={this.handleSubmit}>
-        	<label>
-          	Name:
-          	<input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
-        	</label>
-        	<input type="submit" value="Submit" />
-      	</form>
+    	let synonym;
+	    if (this.state.wordData) {
+	    	synonym = <h4>{this.state.wordData[0].word}</h4>
+	    } else {
+	    	synonym = <h4>Type Your Word Now</h4>
+	    }
+        return (
+        	<div>
+	        <div> 
+		        <form onSubmit={this.handleSubmit}>
+		        	<label>
+		          	Name:
+		          	<input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
+		        	</label>
+		        	<input type="submit" value="Submit" />
+		      	</form>
+		    </div>
+		    <div>
+		    	<h4>{synonym}</h4>
+		    </div>
+		    </div>
         );
     }
 }
